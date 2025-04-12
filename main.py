0b7f22dcdf126d9bdf6115e99074a5a07a96686c
Skykiller099 <dev.skykiller099@gmail.com>
2025-04-12 19:06:57 +0200

    .

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import random
+from languages import 
 computer_choice = random.choice(['rock', 'paper', 'scissors'])
 user_choice = input("Entre ton choix (rock , paper , scissors) ").lower()
-if user_choice not in ['rock', 'paper', 'scissors']:
+if user_choice not in ['rock', 'paper', 'scissors' , 'r', 'p', 's','pierre', 'papier', 'ciseaux']:
+    if user_choice in ['pierre', 'papier', 'ciseaux']:
+        user_choice = user_choice.replace('pierre', 'rock').replace('papier', 'paper').replace('ciseaux', 'scissors')
+    elif user_choice in ['r', 'p', 's']:
+        if user_choice == 'r':
+            user_choice = 'rock'
+        elif user_choice == 'p':
+            user_choice = 'paper'
+        elif user_choice == 's':
+            user_choice = 'scissors'
     print("Choix invalide. Veuillez entrer 'rock', 'paper' ou 'scissors'.")
 else:
-    print(f"Computer chose: {computer_choice}")
+    print(f"Choix de l'ordinateur: {computer_choice}")
     if user_choice == computer_choice:
         print("Match Nul")
     elif (user_choice == 'rock' and computer_choice == 'scissors') or \
